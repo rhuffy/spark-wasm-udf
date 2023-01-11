e219c2728bcd86062620b319c249c88aa2087863
Raymond Huffman <rhuffman@mit.edu>
2023-01-11 00:01:58 -0500

    udf is vectorized

--- demo/udf.c
+++ demo/udf.c
@@ -1,3 +1,12 @@
-int add(int a, int b) {
-    return a + b;
+#include <stdlib.h>
+#include <emscripten.h>
+
+int add(int a0, int a1) {
+    return a0 + a1;
+}
+
+void exec(int *input0, int *input1, int *output, int len) {
+    for (int i = 0; i < len; i++) {
+        output[i] = add(input0[i], input1[i]);
+    }
 }
